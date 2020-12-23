9ca24f8ebba84e26e3272a3a365643396efd73b7
JinOh Kwon <kjo1333@gmail.com>
2020-12-23 23:16:32 +0900

    사용자 개발, 테스트. 운영 db 완료

--- script/init.js
+++ script/init.js
@@ -1,11 +1,39 @@
+print("####################### Start ##############################");
+
+print(
+    "-------------------------- PROD 생성 -----------------------------------"
+);
+
+db = db.getSiblingDB("prod_life");
 db.createUser({
-    user: "lifeUser",
-    pwd: passwordPrompt(),
-    roles: [
-        {
-            role: "userAdminAnyDatabase",
-            db: "admin",
-        },
-        "readWriteAnyDatabase",
-    ],
+    user: "lifeProdUser",
+    pwd: "qwert12345!@",
+    roles: [{ role: "dbOwner", db: "prod_life" }],
 });
+db.createCollection("users");
+
+print(
+    "--------------------------- TEST 생성 -----------------------------------"
+);
+
+db = db.getSiblingDB("test_life");
+db.createUser({
+    user: "lifeTestUser",
+    pwd: "qwert12345!@",
+    roles: [{ role: "dbOwner", db: "test_life" }],
+});
+db.createCollection("users");
+
+print(
+    "-------------------------- DEV 생성 -----------------------------------"
+);
+
+db = db.getSiblingDB("dev_life");
+db.createUser({
+    user: "lifeDevUser",
+    pwd: "qwert12345!@",
+    roles: [{ role: "dbOwner", db: "dev_life" }],
+});
+db.createCollection("users");
+
+print("########################## END ################################");
